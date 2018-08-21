a178a8feaf572f0cac5ed84c488ebe6517300cb5
sauravs1001 <saurav.kec@gmail.com>
2018-08-21 08:40:50 +0530

    from Home Day-2

---
+++ Day-1/keywordsProgram2.py
@@ -0,0 +1,13 @@
+import keyword
+Print(keyword.kwlist)
+
+import keyword
+name=keyword.kwlist
+print(name)
+len(name)
+
+# Using for loop in a list to print the list elements
+import keyword
+name = keyword.kwlist
+for i in name:
+    print(i)
